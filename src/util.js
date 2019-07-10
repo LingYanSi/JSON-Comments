3d2d293c3f48f002be9b61151d2a72104e4e0d39
wangy <1029891106@qq.com>
2019-07-10 16:59:26 +0800

    add: parse comment ; test option

--- src/util.js
+++ src/util.js
@@ -18,3 +18,38 @@ export const getType = item => {
     }
     return item.type
 }
+
+export function testOption(item) {
+    // [[?]] [[$option]]
+    const comments = getComments(item)
+    return parseComment(comments).option
+}
+
+function parseComment(comments = []) {
+    const allDirective = []
+    let option = false
+    let enums = []
+
+    const comment = comments.join('\n').replace(/\[{2}([^\]\n]*)\]{2}/g, (_, $1) => {
+        $1 = $1.trim()
+
+        if (['?', '$option'].includes($1)) {
+            option = true
+        } else if (/^\$.+/.test($1)) {
+            //
+            allDirective.push($1.slice(1))
+        } else {
+            const someEnums = $1.split('|').map(i => i.trim()).filter(i => i)
+            enums.push(...someEnums)
+        }
+        return ''
+    })
+
+    // [[$option]] [[$img]] [[11|222|333|333]] [["sdfsd","sdasdas"]]
+    return {
+        option, // 是不是可选的
+        enums, // 枚举值
+        comment, // 真实注释
+        allDirective,
+    }
+}
